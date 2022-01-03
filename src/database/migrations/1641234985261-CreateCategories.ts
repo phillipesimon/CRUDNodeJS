b0d3a77cce88d527fda53688575ed96291f87f63
phillipesimon <phillipe.simon@gmail.com>
2022-01-03 15:39:12 -0300

    Typeorm configurado

---
+++ src/database/migrations/1641234985261-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategories1641234985261 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
