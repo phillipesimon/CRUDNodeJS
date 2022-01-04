f1128cacefc1bd50c4d699048f4262c4d543f298
phillipesimon <phillipe.simon@gmail.com>
2022-01-04 03:29:06 -0300

    Entities adicionadas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import 'reflect-metadata';
 
 import express from 'express';
 
+import './database';
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => console.log('Server is running on port 3000!!!'));
