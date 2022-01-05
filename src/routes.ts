72e68bf4738df5de1c0cd6767e8854fcab96e4af
phillipesimon <phillipe.simon@gmail.com>
2022-01-05 12:39:54 -0300

    Busca de Categorias ok

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { GetAllCategoriesCotroller } from './controllers/GetAllCategoriesCotroller';
 
 const routes = Router();
 
 routes.post('/categories', new CreateCategoryController().handle);
+routes.get('/categories', new GetAllCategoriesCotroller().handle);
 
 export { routes };
